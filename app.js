48f03720646aa38c78d3153bd3f5a0e0976b8f37
Nicolas Went <nicolas.went@epita.fr>
2021-01-07 15:09:36 +0100

    [AWS][S3] WIP on adding file to S3

--- app.js
+++ app.js
@@ -1,28 +1,55 @@
 const http = require('http');
 const url = require('url');
+const AWS = require('aws-sdk');
+const uuid = require('uuid');
+
+const awsApiVersion = '2006-03-01';
+
+AWS.config.getCredentials(function(err) {
+    if (err)
+        console.log(err.stack);
+});
+
+const bucketName = 'task-store.france-ioi.org';
+
 
 const hostname = '127.0.0.1';
 const port = 3000;
-let conserved = null;
 
 // in req: the user code in json
 // int res: The response we send to the user (url)
 const server = http.createServer((request, response) => {
     // This allow us to enable that user get our response.end after their post in order to send them the link
     response.setHeader('Access-Control-Allow-Origin', '*');
-    response.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    response.setHeader('Access-Control-Allow-Methods', 'GET, POST');
     response.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     response.setHeader('Access-Control-Allow-Credentials', true);
+    // console.log("Access key:", aws.config.credentials.accessKeyId);
+    // console.log("AWS_SECRET_KEY: ", aws.config.credentials.secretAccessKey);
     if (request.method === 'POST') {
         let body = "";
         request.on('data', data => {
             body += data;
         });
         request.on('end', () => {
-            console.log("Body: " + body);
-            conserved = body;
+            // create random key:
+            const keyName = uuid.v4();
+            const bucketPromise = new AWS.S3({apiVersion: awsApiVersion})
+                .createBucket({Bucket: bucketName}).promise();
+
+            bucketPromise.then(
+                function (dataPromise) {
+                    const objectParams = {Bucket: bucketName, Key: keyName, Body: body};
+                    // const objectParams = {Bucket: bucketName, Key: keyName, Body: "Hello World"};
+
+                    const uploadPromise = new AWS.S3({apiVersion: awsApiVersion})
+                        .putObject(objectParams).promise();
+                    uploadPromise.then(dataUpload => console.log("Successfully uploaded data to " + bucketName + "/" + keyName));
+                }
+            ).catch(err => console.error(err, err.stack));
+
             response.writeHead(200, {'Content-Type': 'text/html'});
-            response.end('post received!')
+            response.end(keyName.toString());
         });
     } else {
         const url_parts = url.parse(request.url, true);
