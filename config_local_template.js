99c72c1fb9ddd3af9203548c4ce0e22cf45d6b6a
Nicolas Went <nicolas.went@epita.fr>
2021-01-11 10:43:38 +0100

    [Config] Added configs for database connection

---
+++ config_local_template.js
@@ -0,0 +1,11 @@
+/**
+** Config file for database connection.
+**
+** Change the values there according to the connection to database and rename
+** this file to config_local.js once you have done it.
+*/
+
+const servername = "localhost";
+const username = "username";
+const password = "password";
+const databasename = "quickpiprojects";
